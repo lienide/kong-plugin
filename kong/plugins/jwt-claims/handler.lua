201db36900114c5232a76a9bede12ab0a5893e9d
apple <seul.dos@gmail.com>
2021-06-20 11:04:01 +0800

    feat(kong-plugin): jwt-claims auth with consumer

--- kong/plugins/jwt-claims/handler.lua
+++ kong/plugins/jwt-claims/handler.lua
@@ -7,6 +7,45 @@ local JwtClaimsHandler = BasePlugin:extend()
 JwtClaimsHandler.VERSION = "0.1.0"
 JwtClaimsHandler.PRIORITY = 1005
 
+local function set_consumer(consumer, credential)
+  kong.client.authenticate(consumer, credential)
+
+  local set_header = kong.service.request.set_header
+  local clear_header = kong.service.request.clear_header
+
+  if consumer and consumer.id then
+    set_header(constants.HEADERS.CONSUMER_ID, consumer.id)
+  else
+    clear_header(constants.HEADERS.CONSUMER_ID)
+  end
+
+  if consumer and consumer.custom_id then
+    set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, consumer.custom_id)
+  else
+    clear_header(constants.HEADERS.CONSUMER_CUSTOM_ID)
+  end
+
+  if consumer and consumer.username then
+    set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
+  else
+    clear_header(constants.HEADERS.CONSUMER_USERNAME)
+  end
+
+  if credential and credential.key then
+    set_header(constants.HEADERS.CREDENTIAL_IDENTIFIER, credential.key)
+  else
+    clear_header(constants.HEADERS.CREDENTIAL_IDENTIFIER)
+  end
+
+  clear_header(constants.HEADERS.CREDENTIAL_USERNAME)
+
+  if credential then
+    clear_header(constants.HEADERS.ANONYMOUS)
+  else
+    set_header(constants.HEADERS.ANONYMOUS, true)
+  end
+end
+
 local function retrieve_token(conf)
   local request_headers = kong.request.get_headers()
   for _, v in ipairs(conf.header_names) do
@@ -63,14 +102,20 @@ local function do_authentication(conf)
     return false, { status = 401, message = "Invalid signature" }
   end
 
-  local set_header = kong.service.request.set_header
-  for _, pair in ipairs(conf.claims_to_header) do
-    local old_header, new_header = pair:match("^([^:]+):*(.-)$")
-    local v = claims[old_header]
-    if v then
-      set_header(new_header, v)
-    end
+  -- Retrieve the consumer
+  local consumer_id = claims[conf.jwt_consumer_id]
+  if not consumer_id then
+    return false, { status = 401, message = "Invalid consumer id config" }
   end
+
+  local consumer_cache_key = kong.db.consumers:cache_key(consumer_id)
+  local consumer, err      = kong.cache:get(consumer_cache_key, nil, kong.client.load_consumer, consumer_id, true)
+
+  if err then
+    return error(err)
+  end
+
+  set_consumer(consumer)
 
   return true
 end
